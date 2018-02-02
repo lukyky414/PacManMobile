3d49e8ece43593b63836c6911a52b672fdcd0c98
lukyky414 <lucassschwab.e@gmail.com>
2018-02-02 16:55:13 +0100

    Maze affiché avec pacGomme
    3 = pacPower
    2 = pacGomme
    1 = rien
    0 = mur

--- core/src/com/gdx/game/PacManGdx.java
+++ core/src/com/gdx/game/PacManGdx.java
@@ -9,25 +9,34 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class PacManGdx extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture block;
+	Texture pacGomme;
+	Texture pacPower;
+	Texture dark;
+	Maze wut;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		block = new Texture("bloc.png");
+		pacGomme = new Texture("pellet.png");
+		pacPower = new Texture("superpellet.png");
+		dark = new Texture("dark.png");
+		wut = new Maze(block, pacGomme, pacPower, dark);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(block, 0, 0, 16, 16, 0, 0, 48, 48, false, false);
-		batch.end();
+		wut.drawMaze(batch);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		block.dispose();
+		pacGomme.dispose();
+		pacPower.dispose();
+		dark.dispose();
 	}
 }
--- desktop/src/com/gdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/gdx/game/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		config.title = "PacMan";
 		config.width = 28 * 16;
 		config.height = 31 * 16;
+		
 		new LwjglApplication(new PacManGdx(), config);
 	}
 }
