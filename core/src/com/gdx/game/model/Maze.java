2aa44e073646c0d948da3faf3e3313ae920b9e2f
Nexey <leliam@live.fr>
2018-02-16 15:59:06 +0100

    Refonte classes Maze et World

--- core/src/com/gdx/game/model/Maze.java
+++ core/src/com/gdx/game/model/Maze.java
@@ -3,15 +3,21 @@ package com.gdx.game.model;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.gdx.game.view.TextureFactory;
+import java.util.Iterator;
 
-public class Maze {
+public class Maze implements Iterable<GameElement> {
+	private World _world;
+	private final int _width;
+	private final int _height;
+
 	Texture block=TextureFactory.getInstance().getTextureBloc();
 	Texture pacGomme=TextureFactory.getInstance().getTexturePacGom();
 	Texture pacPower=TextureFactory.getInstance().getTexturePacPower();
 	Texture dark=TextureFactory.getInstance().getTextureDark();
 	Texture pacman = TextureFactory.getInstance().getTexturePacman();
-	
-	int openMaze[][] = {
+
+	/* 0 : mur, 1 : vide, 2 : intersection, 3 : barriere fantomes */
+	private int[][] _laby1 = new int[][] {
 			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 			{0,2,2,2,2,2,2,2,2,2,2,2,2,0,0,2,2,2,2,2,2,2,2,2,2,2,2,0},
 			{0,2,0,0,0,0,2,0,0,0,0,0,2,0,0,2,0,0,0,0,0,2,0,0,0,0,2,0},
@@ -44,9 +50,41 @@ public class Maze {
 			{0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0},
 			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 	};
+	private GameElement[][] _laby2;
 
 	public Maze() {}
-	
+
+	public Maze(World w) {
+		_world = w;
+		this.init();
+	}
+
+	private void init ()
+	{
+		this._height = _laby1.length;
+		this._width  = _laby1[0].length;
+		this._laby2 = new GameElement[this._height][this._width];
+
+		int x = 0,y = 0;
+		for(int[] t : _laby1) {
+			for(int elementType : t) {
+				GameElement element = MazeCOR.getCOR ().build (
+						this._world,
+						elementType,
+						x,
+						y);
+				this._laby2[x][y] = element;
+				y = (++y % this._width);
+			}
+			x++;
+		}
+	}
+
+	public GameElement get(int x, int y) { return this._laby2[x][y]; }
+
+	public int getHeight() { return _height; }
+
+	public int getWidth()  { return _width; }
 	/*public Maze(Texture Block, Texture PacGomme, Texture PacPower, Texture Dark, Texture pacman) {
 		block = Block;
 		pacGomme = PacGomme;
@@ -62,28 +100,68 @@ public class Maze {
 		draw(batch, pacGomme, 2);
 		draw(batch, pacPower, 3);
 		draw(batch, pacman, 4);
-		
+
 	}
-	
+
 	private void draw(SpriteBatch batch, Texture text, int what) {
 		batch.begin();
 		for(int i = 0; i < 28; i ++) {
 			for(int j = 0; j < 31; j++) {
-				if(openMaze[j][i] == what)
+				if(_laby1[j][i] == what)
 					batch.draw(text, i * 16, (30-j) * 16);
 			}
 		}
 		batch.end();
 	}
-	
+
 	private void drawResize(SpriteBatch batch, Texture text, int what) {
 		batch.begin();
 		for(int i = 0; i < 28; i ++) {
 			for(int j = 0; j < 31; j++) {
-				if(openMaze[j][i] == what)
+				if(_laby1[j][i] == what)
 					batch.draw(text, i * 16, (30-j) * 16, 16, 16, 0, 0, 48, 48, false, false);
 			}
 		}
 		batch.end();
+	}
+
+	@Override
+	public Iterator<GameElement> iterator() {
+		return new MazeIterator(this);
+	}
+}
+
+class MazeIterator implements Iterator<GameElement> {
+
+	private Maze _maze;
+	int _i, _j;
+
+	public MazeIterator(Maze maze) {
+		this._maze = maze;
+		_i = _j = 0;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return (_i < this._maze.getHeight()) && (_j < this._maze.getWidth());
+	}
+
+	@Override
+	public GameElement next() {
+		if (!this.hasNext()) throw new NoSuchElementException("No more game elements");
+		GameElement gameElement;
+		do {
+			gameElement = this._maze.get(_i, _j);
+			_j = (_j + 1) % this._maze.getWidth();
+			if (_j == 0)
+				_i++;
+		} while (gameElement == null
+				&& this.hasNext());
+		return gameElement;
+	}
+
+	@Override
+	public void remove() {
+		// TODO
 	}
 }
--- core/src/com/gdx/game/model/World.java
+++ core/src/com/gdx/game/model/World.java
@@ -1,37 +1,74 @@
 package com.gdx.game.model;
 
-import java.util.ArrayList;
+import com.badlogic.gdx.math.Vector2;
 import java.util.Iterator;
 
-public class World implements Iterable<GameElement>{
-    private int height, width;
-    private Maze labyrinthe;
-    private Pacman pacman;
+public class World implements Iterable<GameElement> {
+    private Maze _maze;
+    private Pacman _pacman;
 
-    public World()
-    {
-
+    public World() {
+        this._pacman = new Pacman(new Vector2(1, 1), this);
+        this._maze = new Maze(this);
     }
 
     public int getHeight() {
-        return height;
+        return this.getHeight();
     }
 
     public int getWidth() {
-        return width;
+        return this.getWidth();
     }
 
-    public Maze getLabyrinthe() {
-        return labyrinthe;
+    public Maze getMaze() {
+        return _maze;
     }
 
     public Pacman getPacman() {
-        return pacman;
+        return _pacman;
     }
 
     @Override
     public Iterator<GameElement> iterator() {
-        ArrayList<GameElement> l = new ArrayList<GameElement>();
-        return l.iterator();
+        return new WorldIterator(this);
+    }
+}
+
+class WorldIterator implements Iterator<GameElement>
+{
+    private World _world;
+    private Iterator<GameElement> _mazeIterator;
+    int _i;
+
+    public WorldIterator(World world) {
+        this._world = world;
+        this._mazeIterator = this._world.getMaze().iterator();
+        this._i = 0; /* 0 = maze, 1 = pacman */
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (this._i < 1);
+    }
+
+    @Override
+    public GameElement next() {
+        if((_i == 0)) {
+            if (!this._mazeIterator.hasNext())
+                _i = 1; // on passe à Pacman
+        }
+        else _i++;
+
+        GameElement res;
+        switch(this._i) {
+            case 0 : return this._mazeIterator.next();
+            case 1 : return this._world.getPacman();
+            default : return null; /* erreur */
+        }
+    }
+
+    @Override
+    public void remove() {
+        // TODO
     }
 }
